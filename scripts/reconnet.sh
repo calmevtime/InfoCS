9e81a612c645e056146c7addc41cea8c9ae613eb
kaixu <calmevtime@gmail.com>
2019-05-05 17:05:56 -0700

    April 30, 2019 Updates

--- scripts/reconnet.sh
+++ scripts/reconnet.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p ../results/cifar10/cr10/reconnet_basic/ && cd .. && python main_reconnet.py --cr 10 | tee results/cifar10/cr10/reconnet_basic/log.log &&
-mkdir -p ../results/cifar10/cr20/reconnet_basic/ && cd .. && python main_reconnet.py --cr 20 | tee results/cifar10/cr20/reconnet_basic/log.log &&
-mkdir -p ../results/cifar10/cr40/reconnet_basic/ && cd .. && python main_reconnet.py --cr 40 | tee results/cifar10/cr40/reconnet_basic/log.log &&
+mkdir -p ../results/cifar10/cr10/reconnet_basic/ && cd .. && python main_reconnet.py --cr 10 | tee results/cifar10/cr10/reconnet_basic/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr20/reconnet_basic/ && cd .. && python main_reconnet.py --cr 20 | tee results/cifar10/cr20/reconnet_basic/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr40/reconnet_basic/ && cd .. && python main_reconnet.py --cr 40 | tee results/cifar10/cr40/reconnet_basic/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr80/reconnet_basic/ && cd .. && python main_reconnet.py --cr 80 | tee results/cifar10/cr80/reconnet_basic/log.log
