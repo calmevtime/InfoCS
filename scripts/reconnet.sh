a1265df02853fec97f9334ad585f2771b4ea75d7
kaixu <calmevtime@gmail.com>
2019-05-06 21:25:48 -0700

    May 06, 2019 Updates

--- scripts/reconnet.sh
+++ scripts/reconnet.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+mkdir -p ../results/cifar10/cr5/reconnet_basic/ && cd .. && python main_reconnet.py --cr 5 | tee results/cifar10/cr5/reconnet_basic/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr10/reconnet_basic/ && cd .. && python main_reconnet.py --cr 10 | tee results/cifar10/cr10/reconnet_basic/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr20/reconnet_basic/ && cd .. && python main_reconnet.py --cr 20 | tee results/cifar10/cr20/reconnet_basic/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr30/reconnet_basic/ && cd .. && python main_reconnet.py --cr 30 | tee results/cifar10/cr30/reconnet_basic/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr40/reconnet_basic/ && cd .. && python main_reconnet.py --cr 40 | tee results/cifar10/cr40/reconnet_basic/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr50/reconnet_basic/ && cd .. && python main_reconnet.py --cr 50 | tee results/cifar10/cr50/reconnet_basic/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr60/reconnet_basic/ && cd .. && python main_reconnet.py --cr 60 | tee results/cifar10/cr60/reconnet_basic/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr70/reconnet_basic/ && cd .. && python main_reconnet.py --cr 70 | tee results/cifar10/cr70/reconnet_basic/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr80/reconnet_basic/ && cd .. && python main_reconnet.py --cr 80 | tee results/cifar10/cr80/reconnet_basic/log.log
--- scripts/reconnet_sparse.sh
+++ scripts/reconnet_sparse.sh
@@ -5,5 +5,6 @@ mkdir -p ../results/cifar10/cr20/reconnet_sparse/ && cd .. && python main_reconn
 mkdir -p ../results/cifar10/cr30/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 30 | tee results/cifar10/cr30/reconnet_sparse/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr40/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 40 | tee results/cifar10/cr40/reconnet_sparse/log.log && cd scripts &&
 mkdir -p ../results/cifar10/cr50/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 50 | tee results/cifar10/cr50/reconnet_sparse/log.log && cd scripts &&
-mkdir -p ../results/cifar10/cr60/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 60 | tee results/cifar10/cr60/reconnet_sparse/log.log && cd scripts
-#mkdir -p ../results/cifar10/cr80/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 80 | tee results/cifar10/cr80/reconnet_sparse/log.log
+mkdir -p ../results/cifar10/cr60/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 60 | tee results/cifar10/cr60/reconnet_sparse/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr70/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 70 | tee results/cifar10/cr70/reconnet_sparse/log.log && cd scripts &&
+mkdir -p ../results/cifar10/cr80/reconnet_sparse/ && cd .. && python main_reconnet_sparse.py --cr 80 | tee results/cifar10/cr80/reconnet_sparse/log.log
